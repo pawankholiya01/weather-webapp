4d7bd0ff5bbe71452f52cb3e6813202cb8afc962
pawankholiya01 <49568795+pawankholiya01@users.noreply.github.com>
2020-09-21 21:05:09 +0530

    removed redundat code and comments

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 const request = require("request");
 const geocode = require("./geocode")
 const weather = require("./forecast")
-
-// const url = "http://api.weatherstack.com/current?access_key=4f81e6e489634a98002597728d7b4ac3&query=37.8267,-122.4233"
-
-// request({ url : url , json : true },(error,response) =>  {
-  
-//     // console.log(response.body.current);
-//     console.log(response.body.current.weather_descriptions[0] +  " The current temprature is : " , response.body.current.temperature , "degree and it feels like " , response.body.current.feelslike,"degree.");
-
-// });
-
-
-
 weather({
     latitute : 0,
     longitude : 0
