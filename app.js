0ca1c3186d903b927b67b413d5f56c25f416557e
pawankholiya01 <49568795+pawankholiya01@users.noreply.github.com>
2020-09-21 20:56:32 +0530

    working geocode and forecast api's

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const geocode = require("./geocode")
 
 // const url = "http://api.weatherstack.com/current?access_key=4f81e6e489634a98002597728d7b4ac3&query=37.8267,-122.4233"
 
@@ -54,30 +55,6 @@ weather({
 
 
 
-const geocode = (address,callback) => {
-const mapurl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURI(address)  + ".json?access_token=pk.eyJ1IjoicGF3YW5raG9saXlhMDEiLCJhIjoiY2tmOWRpMDFzMGwxbDJ6cXNiODNzODRxZyJ9.fF030x7YhuJQQ5JoQpts5A&limit=1"
-request({ url: mapurl, json: true }, (error, response) => {
-    // console.log(response);
-    if(error)
-    {
-        // console.log(" Error can't access API ");
-        callback("Error can't access API", undefined );
-    }
-    else if (response.body.features===undefined){
-        callback("Please Enter a valid Address", undefined);
-        // console.log(" Please enter a valid search text ");
-    }
-    else{
-        callback( undefined, {
-            latitute: response.body.features[0].center[1],
-            longitude: response.body.features[0].center[0]
-        });
-        // console.log(" Latitude is : ", response.body.features[0].center[1], "and Longitude is : ", response.body.features[0].center[0]);
-    }
-
-});
-
-}
 
 geocode("Khatima",(error,response)=>{
     if(error)
