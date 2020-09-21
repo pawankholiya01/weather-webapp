0cd4ad5e8fc384477cc48d098180d59b4a04e425
pawankholiya01 <49568795+pawankholiya01@users.noreply.github.com>
2020-09-21 21:01:05 +0530

    sepregated forecast module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const geocode = require("./geocode")
+const weather = require("./forecast")
 
 // const url = "http://api.weatherstack.com/current?access_key=4f81e6e489634a98002597728d7b4ac3&query=37.8267,-122.4233"
 
@@ -11,32 +12,7 @@ const geocode = require("./geocode")
 // });
 
 
-const weather = (location, callback) => {
-    console.log(location);
-    const url = "http://api.weatherstack.com/current?access_key=4f81e6e489634a98002597728d7b4ac3&query=" + location.latitute + ',' + location.longitude;
-    console.log(url);
-    request({ url: url, json: true }, (error, response) => {
-        // console.log(response);
-        if (error) {
-            // console.log(" Error can't access API ");
-            callback("Error can't access API", undefined);
-        }
-        else if (response.body.current === undefined) {
-            callback("Please Enter a valid Location", undefined);
-            console.log(response.body);
 
-        }
-        else {
-            callback(undefined, {
-                temprature: response.body.current.temperature,
-                weather_despcription: response.body.current.weather_descriptions[0]
-            });
-            // console.log(" Latitude is : ", response.body.features[0].center[1], "and Longitude is : ", response.body.features[0].center[0]);
-        }
-
-    });
-
-}
 weather({
     latitute : 0,
     longitude : 0
@@ -50,11 +26,6 @@ weather({
     }
 
 })
-
-
-
-
-
 
 geocode("Khatima",(error,response)=>{
     if(error)
---
+++ forecast.js
@@ -0,0 +1,31 @@
+request = require("request");
+
+
+const weather = (location, callback) => {
+    console.log(location);
+    const url = "http://api.weatherstack.com/current?access_key=4f81e6e489634a98002597728d7b4ac3&query=" + location.latitute + ',' + location.longitude;
+    console.log(url);
+    request({ url: url, json: true }, (error, response) => {
+        // console.log(response);
+        if (error) {
+            // console.log(" Error can't access API ");
+            callback("Error can't access API", undefined);
+        }
+        else if (response.body.current === undefined) {
+            callback("Please Enter a valid Location", undefined);
+            console.log(response.body);
+
+        }
+        else {
+            callback(undefined, {
+                temprature: response.body.current.temperature,
+                weather_despcription: response.body.current.weather_descriptions[0]
+            });
+            // console.log(" Latitude is : ", response.body.features[0].center[1], "and Longitude is : ", response.body.features[0].center[0]);
+        }
+
+    });
+}
+
+
+module.exports = weather;
